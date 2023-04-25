73912016e0694aa00a2a9293fe2b5247e9e5bf2e
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-25 15:07:15 +0200

    feature: add generic service

---
+++ src/interceptors/LoginInterceptor.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import GenericService from '../service/GenericService';
+
+const customAxios = axios.create();
+export const customInterceptor = (
+  navigate: any,
+  toggleChangedLocalStorage: any
+) => {
+  customAxios.interceptors.response.use(
+    async function (response) {
+      //   GenericService.refreshToken(response.data);
+      return response;
+    },
+    function (error) {
+      if (error.response.status === 401) {
+        console.log('Unauthorized');
+        localStorage.clear();
+        toggleChangedLocalStorage();
+        navigate('/login');
+      }
+      return Promise.reject(error);
+    }
+  );
+};
+
+customAxios.interceptors.request.use(
+  (config: any) => {
+    let tokensData = localStorage.getItem('token');
+    config.headers['Authorization'] = `${tokensData}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error.message);
+  }
+);
+
+export default customAxios;
---
+++ src/service/GenericService.ts
@@ -0,0 +1,147 @@
+import Result from '../dto/Result';
+import customAxios from '../interceptors/LoginInterceptor';
+
+export default class GenericService {
+  private static baseUrl: string =
+    'http://' +
+    process.env.REACT_APP_URI +
+    ':' +
+    process.env.REACT_APP_PORT +
+    '/';
+
+  public static async getAll<T>(modelName: string): Promise<T> {
+    return await customAxios
+      .get<Array<T>>(this.baseUrl + modelName, { withCredentials: true })
+      .then(async (result: any) => {
+        return result.data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async getById<T>(modelName: string, id: number): Promise<T> {
+    return await customAxios
+      .get<Array<T>>(`${this.baseUrl}${modelName}/${id}`, {
+        withCredentials: true,
+      })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async get<T>(modelName: string): Promise<T> {
+    return await customAxios
+      .get<Array<T>>(`${this.baseUrl}${modelName}`, {
+        withCredentials: true,
+      })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async create<T>(
+    modelName: string,
+    data: T
+  ): Promise<Result<T>> {
+    return await customAxios
+      .post<T>(`${this.baseUrl}${modelName}`, data, { withCredentials: true })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async createDifResponse<T, U>(
+    modelName: string,
+    data: T
+  ): Promise<Result<U>> {
+    return await customAxios
+      .post<T>(`${this.baseUrl}${modelName}`, data, { withCredentials: true })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async post<T>(modelName: string): Promise<Result<T>> {
+    return await customAxios
+      .post<T>(`${this.baseUrl}${modelName}`, '', { withCredentials: true })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async update<T, S>(
+    modelName: string,
+    id: number,
+    data: T
+  ): Promise<Result<S>> {
+    return await customAxios
+      .put<T>(`${this.baseUrl}${modelName}/${id}`, data, {
+        withCredentials: true,
+      })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async delete<T>(modelName: string, id: number): Promise<T> {
+    return await customAxios
+      .delete(`${this.baseUrl}${modelName}/${id}`, { withCredentials: true })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  public static async patch<T>(modelName: string, id: number): Promise<T> {
+    return await customAxios
+      .patch(`${this.baseUrl}${modelName}/${id}`, { withCredentials: true })
+      .then(async (result: any) => {
+        let data = result.data;
+        return data;
+      })
+      .catch((err: any) => {
+        throw err;
+      });
+  }
+
+  //   public static refreshToken(result: Result<any>) {
+  //     if (result.refresh_token) {
+  //       GenericService.createDifResponse<JwtI, JwtI>('user/refreshToken', {
+  //         jwt: localStorage.getItem('token') || '',
+  //       }).then((response) => {
+  //         if (response.success) {
+  //           localStorage.setItem('token', response.result.jwt);
+  //         }
+  //       });
+  //     } else {
+  //     }
+  //   }
+}
