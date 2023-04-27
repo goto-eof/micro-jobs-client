7de76665afd95720188b1c38308ea2356940bd7e
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-27 21:35:33 +0200

    chore: style changes

--- src/components/OffersRequests.tsx
+++ src/components/OffersRequests.tsx
@@ -8,6 +8,9 @@ import {
   Image,
   Text,
   Box,
+  Fade,
+  ScaleFade,
+  Skeleton,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import GenericService from '../service/GenericService';
@@ -24,12 +27,15 @@ interface Props {
 export default function OffersRequests({ baseUrl, urlCountItems }: Props) {
   const [offers, setOffers] = useState<Array<Job>>(new Array<Job>());
   const [itemsCount, setItemsCount] = useState(0);
+  const [isLoaded, setLoaded] = useState(false);
+
   useEffect(() => {
     GenericService.getAll<Array<Job>>(baseUrl + '/0').then((data) => {
       GenericService.get<GenericResponse<number>>(urlCountItems).then(
         (genericResponse) => {
           setOffers(data);
           setItemsCount(genericResponse.value);
+          setLoaded(true);
         }
       );
     });
@@ -68,29 +74,31 @@ interface Offer {
 }
 
 function Offer({ title, description }: Offer) {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <Card
       direction={{ base: 'column', sm: 'row' }}
       overflow="hidden"
       variant="outline"
     >
-      <Image
-        objectFit="cover"
-        maxW={{ base: '100%', sm: '200px' }}
-        src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-        alt="Caffe Latte"
-      />
-
+      <Skeleton width={{ base: '100%', sm: '200px' }} isLoaded={imageLoaded}>
+        <Image
+          objectFit="cover"
+          maxW={{ base: '100%', sm: '200px' }}
+          src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+          alt="Caffe Latte"
+          onLoad={() => setImageLoaded(true)}
+        />
+      </Skeleton>
       <Stack>
         <CardBody>
           <Heading size="md">{title}</Heading>
 
           <Text py="2">{description}</Text>
         </CardBody>
-
         <CardFooter>
           <Button variant="solid" colorScheme="blue">
-            {title}
+            Accept
           </Button>
         </CardFooter>
       </Stack>
