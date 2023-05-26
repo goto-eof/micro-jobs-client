9effc23b76df9b9db6830ca683b8eed3a59652db
Andrei Dodu <andrei.dodu@hotmail.it>
2023-05-26 22:19:52 +0200

    refactor

--- src/Main.tsx
+++ src/Main.tsx
@@ -213,7 +213,7 @@ export default function Main() {
                   })}{' '}
                   <MenuDivider />
                   <MenuItem onClick={() => switchColor()}>
-                    Switch color
+                    Switch theme
                   </MenuItem>
                   <MenuDivider />
                   <MenuItem
